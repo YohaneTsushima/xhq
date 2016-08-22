bea49145b21edb79aa99bc4823c35e2dd7d189c4
megasheep <megasheep@caafdd74-77ec-4ac3-adaf-26f2927652f1>
2016-08-22 04:05:36 +0000

    start

---
+++ Aqours/src/main/java/com/aq/test/Test1.java
@@ -0,0 +1,27 @@
+package com.aq.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Test1 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
